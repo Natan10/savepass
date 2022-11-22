517b6f7e9f7e463dd6b473329a0b09ec4efdc72e
Natan moreira <natan-moreira1@hotmail.com>
2022-11-22 10:14:37 -0300

    feat: create Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { AntDesign } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import { 
+  Container,
+  Header,
+  UserContainer,
+  UserPhoto,
+  UserInfo,
+  Title,
+  Bold,
+  Subtitle,
+  Button
+} from './styles';
+import { useTheme } from 'styled-components';
 
 const Home = () => {
+
+  const theme = useTheme();
+
   return(
-    <Container></Container>
+    <Container>
+      <Header>
+        <UserContainer>
+          <UserPhoto
+            resizeMode='cover'
+            source={{
+              uri: 'https://doodleipsum.com/700/avatar?bg=FF3C3C&i=3cdf1e1375692dbe37689851492c2957'
+            }} 
+          />
+          <UserInfo>
+            <Title numberOfLines={1}>
+              Olá, <Bold>Rocketseat</Bold>
+            </Title>
+            <Subtitle>Sinta-se seguro aqui</Subtitle>
+          </UserInfo>
+        </UserContainer>
+        <Button>
+          <Subtitle>
+            <AntDesign name="plus" size={16} color={theme.colors.white}/>
+          </Subtitle>
+        </Button>
+      </Header>
+    </Container>
   );
 }
 
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,61 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
 
 export const Container = styled.View`
+  flex: 1;
+  background-color: ${({theme}) => theme.colors.bg};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 180px;
+  background-color: ${({theme}) => theme.colors.primary};
+
+  padding-top: ${getStatusBarHeight()};
+  padding: 0px 24px;
   
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+
+export const UserContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+`;
+
+export const UserPhoto = styled.Image`
+  width: 55px;
+  height: 55px;
+  border-radius: 5px;
+`;
+
+export const UserInfo = styled.View`
+  max-width: 200px;
+
+  margin-left: 12px;
+`;
+
+export const Title = styled.Text`
+  color: ${({theme}) => theme.colors.white};
+  font-size: 16px;
+  font-family: ${({theme}) => theme.fonts.light};
+`;
+
+export const Bold = styled.Text`
+  font-family: ${({theme}) => theme.fonts.bold};
+`;
+
+export const Subtitle = styled.Text`
+  color: ${({theme}) => theme.colors.white};
+  font-family: ${({theme}) => theme.fonts.light};
+`;
+
+export const Button = styled.TouchableOpacity`
+  padding: 12px;
+  border: 1px solid white;
+  border-radius: 5px;
 `;

d647056db5915a72b4e21e9e5c7ace1937d92814
Natan moreira <natan-moreira1@hotmail.com>
2022-11-22 09:33:13 -0300

    feat: create theme and import fonts

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Rubik_300Light,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_700Bold
+} from '@expo-google-fonts/rubik';
 
+import Home from './src/screens/Home';
+
+import theme from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Rubik_300Light,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold
+  });
+
+  if(!fontsLoaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    primary: '#1967FB',
+    bg: '#F2F3F5',
+    white: '#FFF',
+    black: '#000',
+    text: '#3E444E',
+    text_light: '#9EA2AA'
+  },
+  fonts: {
+    light: 'Rubik_300Light',
+    regular: 'Rubik_400Regular',
+    medium: 'Rubik_500Medium',
+    bold: 'Rubik_700Bold'
+  }
+}
